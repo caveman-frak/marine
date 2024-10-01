48596672a4460d8d0f5179667a2cd7a99884587d
Caveman Frak <tim.pickard@bluegecko.co.uk>
2024-10-01 09:39:42 +0100

    upgrade to JDK 23

--- buildSrc/src/main/kotlin/marine.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/marine.java-conventions.gradle.kts
@@ -16,11 +16,11 @@ description = "Marine Tracking System"
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(22))
+        languageVersion.set(JavaLanguageVersion.of(23))
         vendor.set(JvmVendorSpec.ORACLE)
     }
-    sourceCompatibility = JavaVersion.VERSION_22
-    targetCompatibility = JavaVersion.VERSION_22
+    sourceCompatibility = JavaVersion.VERSION_23
+    targetCompatibility = JavaVersion.VERSION_23
 }
 
 repositories {
